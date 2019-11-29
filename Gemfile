0d006e0778b309921327cf5bc4e0102968c9d264
yudai <fukuta.yudai@gmail.com>
2019-11-30 01:36:59 +0900

    add-3S-system

--- Gemfile
+++ Gemfile
@@ -58,3 +58,5 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
 gem 'carrierwave'
 gem 'mini_magick'
+gem 'fog-aws'
+gem 'pry-rails'
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -11,7 +11,7 @@ class PostsController < ApplicationController
   def create
     Post.create(post_params)
   end
-  
+
   private
   def post_params
     params.require(:post).permit(:content, :image)
