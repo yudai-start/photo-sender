5c8a5de4968545d435b9d0d82ce7bc561a7449ce
yudai <fukuta.yudai@gmail.com>
2019-12-01 19:16:01 +0900

    modify-profile-model

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,22 +1,3 @@
 class Profile < ApplicationRecord
   mount_uploader :image, ProfileUploader
-
-  # def self.index_faces(image_path)
-  #   res = client.index_faces({
-  #     collection_id: profile-photos,
-  #     image: {
-  #       bytes: image_bytes,
-  #       s3_object: {
-  #         bucket: "photo-sender-posts",
-  #         name: "uploads/profile/image/#{image_path}",
-  #       },
-  #     },
-  #   })
-
-    # face_id = res.to_h[:face_records].map { |face| [face[:face][:face_id]]}.to_h
-    
-  # end
-
-  # has_many :posts
-  
 end
